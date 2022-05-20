e8e7b98535e2b687373771a5122e629b9f26aa0c
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-20 22:21:49 +0600

    Updating

--- main.py
+++ main.py
@@ -1,21 +1,16 @@
 from tkinter import *
 
-window = Tk()
+window = Tk("Miles to Kilometer converter")
 window.minsize(width=300, height=50)
+window.config(pady=20, padx=50)
 
 # Entry
 input = Entry()
 input.grid(row=0, column=1)
 
-
-
-
-
-
-
-
-
-
-
+# label
+label = Label()
+label.config(text="Miles", font=("Arial", 12, "bold"), padx=15)
+label.grid(row=0, column=2)
 
 window.mainloop()
