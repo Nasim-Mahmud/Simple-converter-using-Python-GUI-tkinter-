32c9372df315fddc20d71b271c2f7058f4084785
Nasim Mahmud <nasim.mishu@northsouth.edu>
2022-05-22 19:54:44 +0600

    Finalizing.

--- main.py
+++ main.py
@@ -39,6 +39,8 @@ result = Label()
 result.config(font=("Arial", 12, "bold"))
 result.grid(row=1, column=1)
 
-
+# Button
+button = Button(text="Calculate", command=converter, font=("Arial", 12, "bold"))
+button.grid(row=2, column=1)
 
 window.mainloop()
